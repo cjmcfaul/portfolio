1c9338ea90b9bd4c20c67ef81b647bbb7104cff1
cjm@colinmcfaul.com <cjm@colinmcfaul.com>
2024-06-05 12:10:26 -0400

    remove user urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -13,8 +13,6 @@ urlpatterns = [
     path("resume/", TemplateView.as_view(template_name="pages/resume.html"), name="resume"),
     path("contact/", TemplateView.as_view(template_name="pages/contact.html"), name="contact"),
     path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
-    # User management
-    path("users/", include("apps.users.urls", namespace="users")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
