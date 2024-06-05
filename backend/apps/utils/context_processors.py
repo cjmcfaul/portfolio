12e3a892b369cf1780ec804c0e93a75a6cb4efbe
cjm@colinmcfaul.com <cjm@colinmcfaul.com>
2024-06-05 12:20:49 -0400

    remove user urls

--- backend/apps/utils/context_processors.py
+++ backend/apps/utils/context_processors.py
@@ -1,9 +1,9 @@
 from django.conf import settings
-# from django.middleware import csrf
+from django.middleware import csrf
 
 
 def htmx_context(_request):
     return {
         "WEBPACK_ENABLED": False, # settings.DEBUG,
-        "csrf_token": "", # csrf.get_token(_request)
+        "csrf_token": csrf.get_token(_request)
     }
